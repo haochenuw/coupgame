f7d843c4b3f2a12933a3701eea9856001ba32645
Hao Chen <sxxach@gmail.com>
2021-05-22 00:18:59 -0700

    card types

--- types.ts
+++ types.ts
@@ -5,9 +5,32 @@ export type RoomStatus =  {
     numPlayers: number; 
 }
 
+export type Card = {
+    name: CardName, 
+    action: Action | null, 
+    blocksAction: Action | null, 
+    isRevealed: boolean // if card is already revealed. 
+}
+
+enum CardName {
+    Duke,
+    Ambassador,
+    Assassin,
+    Captain,
+}
+
+enum Action {
+    Tax,
+    Down,
+    Left,
+    Right,
+}
+
+const allCards = []; 
+
 export type GameState = {
     activePlayer: number, 
-    playerOneState: string,
-    playerTwoState: string,
-    deckState: string, 
+    playerOneState: Array<Card>,
+    playerTwoState: Array<Card>,
+    deckState: Array<Card>, 
 }
