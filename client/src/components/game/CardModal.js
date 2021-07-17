b5679479ea3bd3503fa5bdf3ef25dd0ffdc16264
Hao Chen <sxxach@gmail.com>
2021-07-17 12:36:54 -0700

    complete card modal

--- client/src/components/game/CardModal.js
+++ client/src/components/game/CardModal.js
@@ -7,19 +7,31 @@ const getAction = (card) => {
             return "Tax"
         case "Captain":
             return "Steal"
-        default: 
+        case "Ambassador":
+            return "Exchange"
+        case "Assassin":
+            return "Assasinate"
+        case "Contessa":
             return "None"
+        default: 
+            return "???"
     }
 }; 
 
 const getBlock = (card) => {
     switch (card) {
         case "Duke":
-            return "Tax"
+            return "ForeignAid"
         case "Captain":
             return "Steal"
-        default: 
+        case "Ambassador":
+            return "Steal"
+        case "Assassin":
             return "None"
+        case "Contessa":
+            return "Assasinate"
+        default: 
+            return "???"
     }
 }; 
 
