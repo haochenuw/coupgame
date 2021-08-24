2530c67935350e237a1ff1938c9d4b42f65cddfa
Hao Chen <sxxach@gmail.com>
2021-08-24 09:17:33 -0700

    working on local storage solution

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,19 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import {withRouter} from "react-router-dom";
+import {useStateWithLocalStorage} from "./hooks/useStateWithLocalStorage"
 
 const axios = require('axios');
 // const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3002"
 
 export const Create = withRouter(({history}) => {
-    // console.log("history", history); 
-    // console.log("location", location); 
-    // console.log("match", match); 
 
     const [disabled, setDisabled] = useState(false)
 
     const [name, setName] = useState(null)
 
-    const handleChange = (e) => setName(e.target.value);
+    const [nameRegistered, setNameRegistered] = useStateWithLocalStorage(
+        'nameIsRegistered'
+    );
+
 
     function createParty(){
         // console.log('create button clicked'); 
@@ -30,6 +31,7 @@ export const Create = withRouter(({history}) => {
     }
 
     useEffect(()=>{
+        setNameRegistered(false); 
         createParty(); 
     }, []); 
 
--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,95 +1,102 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import MainGame from "./game/MainGame"
 import './styles/buttons.css';
 import './styles/styles.css';
 import io from "socket.io-client";
-import RulesModal from "./RulesModal"; 
+import RulesModal from "./RulesModal";
+import {useStateWithLocalStorage} from "./hooks/useStateWithLocalStorage"
 
 export const SocketContext = React.createContext()
 
 // const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3002"
 
-let socket = null; 
+let socket = null;
 
-const MAX_PLAYERS = 6; 
-const MIN_PLAYERS = 2; 
+const MAX_PLAYERS = 6;
+const MIN_PLAYERS = 2;
 
-export default function Room({history, match, location}) {
+export default function Room({ history, match, location }) {
     // Connect through socket. 
-    console.log(`got room name = ${match.params.name}`); 
-    if (socket === null){
-        console.log('connecting to socket.io...'); 
+    console.log(`got room name = ${match.params.name}`);
+    if (socket === null) {
+        console.log('connecting to socket.io...');
         socket = io(`/${match.params.name}`);
     }
-    // if (location.state === undefined || location.state.playerName === undefined){
-    //     history.push({pathname:`/join`});
-    // }
 
-    const [players, setPlayers] = useState(null); 
-    const [name, setName] = useState(null); 
-    const [me, setMe] = useState(''); 
-    const [winner, setWinner] = useState(null); 
+    const [players, setPlayers] = useState(null);
+    const [me, setMe] = useState('');
+    const [name, setName] = useStateWithLocalStorage(
+        'playerName'
+    );
+    const [nameRegistered, setNameRegistered] = useStateWithLocalStorage(
+        'nameIsRegistered'
+    );
+    const [winner, setWinner] = useState(null);
     const [roomStatus, setRoomStatus] = useState('NOT_READY_TO_START');
     const [initialState, setInitialState] = useState(null);
-    
+
     useEffect(() => {
         socket.on("connect", () => {
-            setMe(socket.id); 
+            setMe(socket.id);
+            if (nameRegistered){
+                console.log("re-setting the name");
+                socket.emit('setName', name);
+            }
         });
 
         socket.on("roomFull", () => {
-            setRoomStatus("ROOM_FULL"); 
+            setRoomStatus("ROOM_FULL");
         });
 
         socket.on("gameInProgress", () => {
-            setRoomStatus("GAME_IN_PROGRESS"); 
+            setRoomStatus("GAME_IN_PROGRESS");
         });
 
         socket.on("playersUpdate", (players) => {
             console.log('got players update', players);
-            setPlayers(players); 
-        }); 
+            setPlayers(players);
+        });
 
-        socket.on('startGameResponse', (initialStateFromServer)=>{
+        socket.on('startGameResponse', (initialStateFromServer) => {
             console.log('got start game response');
-            setInitialState(initialStateFromServer); 
+            setInitialState(initialStateFromServer);
             setRoomStatus("STARTED");
         });
 
-        socket.on("initialState", state =>{
+        socket.on("initialState", state => {
             console.log('state', JSON.stringify(state));
         });
 
         socket.on("gameOver", winner => {
             console.log('game over: winner is', winner);
-            setWinner(winner); 
-            setRoomStatus("GAMEOVER"); 
+            setWinner(winner);
+            setRoomStatus("GAMEOVER");
         });
-    }, []); 
+    }, []);
 
-    function setReady(){
+    function setReady() {
         setPlayers(
-            players.map(item => 
-                item.client_id === me 
-                ? {...item, isReady : true} 
-                : item 
-        ));
-        socket.emit('playerReady'); 
+            players.map(item =>
+                item.client_id === me
+                    ? { ...item, isReady: true }
+                    : item
+            ));
+        socket.emit('playerReady');
         setRoomStatus('WAIT_FOR_OTHERS')
     }
 
-    function isAllPlayersReady(){
-        if (players === null){
-            return false; 
+    function isAllPlayersReady() {
+        if (players === null) {
+            return false;
         }
         return players.every(player => player.isReady);
     }
 
-    function isPlayerNumberAllowed(){
-        if (players === null){
-            return false; 
+    function isPlayerNumberAllowed() {
+        if (players === null) {
+            return false;
         }
-        return players.length <= MAX_PLAYERS && players.length >= MIN_PLAYERS;  
+        return players.length <= MAX_PLAYERS && players.length >= MIN_PLAYERS;
     }
 
     function startGame() {
@@ -97,52 +104,55 @@ export default function Room({history, match, location}) {
     }
 
     function isCreator() {
-        if (location.state !== undefined && location.state.data !== undefined){
+        if (location.state !== undefined && location.state.data !== undefined) {
             return location.state.data
-        } 
-        return false; 
+        }
+        return false;
     }
 
     function gameOverPanel() {
         return (
             <div>
-                <h2>Game over: winner is {winner}</h2> 
+                <h2>Game over: winner is {winner}</h2>
                 {winner === name && (<h2>You won!</h2>)}
                 {winner !== name && (<h2>You lost!</h2>)}
             </div>
         )
     }
 
-    function onSetName(value){
+    function onSaveName(value) {
         setName(value)
-        console.log(`Got player name = ${value}`); 
-        socket.emit('setName', value); 
+        console.log(`Got player name = ${value}`);
+        socket.emit('setName', value);
+        setNameRegistered(true); 
     }
 
-    function setNamePanel(){
-        return(
+    const onChange = event => setName(event.target.value);
+
+    function setNamePanel() {
+        return (
             <div className="joinHome">
-            <input id='inputName' value={name} type="text" placeholder="Your Name"/>
-            <button className="btn btn-info" onClick={() => onSetName(document.getElementById('inputName').value)}>Save</button>
+                <input id='inputName' value={name} type="text" placeholder="Your Name" onChange={onChange} />
+                <button className="btn btn-info" onClick={() => onSaveName(document.getElementById('inputName').value)}>Save</button>
             </div>
         )
     }
 
-    function canStartGame(){
+    function canStartGame() {
         return isAllPlayersReady() && isPlayerNumberAllowed() && isCreator() === true
     }
 
-    if (name === null){
-        return setNamePanel() // TODO 
-    } 
+    if (name === "" || nameRegistered === false) {
+        return setNamePanel()
+    }
 
-    return(
-            <div className="roomHome">
-                <div class="roomHeader">
-                    <div className="roomName"> ROOM {match.params.name} </div>
-                    <RulesModal style="small"/>
-                </div>
-                <div className="readyAndStart">
+    return (
+        <div className="roomHome">
+            <div class="roomHeader">
+                <div className="roomName"> ROOM {match.params.name} </div>
+                <RulesModal style="small" />
+            </div>
+            <div className="readyAndStart">
                 {
                     roomStatus === 'ROOM_FULL' &&
                     <h2>Room is full!</h2>
@@ -160,78 +170,51 @@ export default function Room({history, match, location}) {
                     isCreator() === true &&
                     <button className="btn btn-success" disabled={!canStartGame()} onClick={startGame}>Start Game</button>
                 }
-                </div>
-                {
-                    roomStatus === 'STARTED' &&
-                    initialState !== null &&
-                    <SocketContext.Provider value={socket}>
-                    <MainGame me = {me} myName ={name} initialState={initialState}/>
-                    </SocketContext.Provider>
-                }
-                { 
-                    roomStatus !== 'STARTED' && roomStatus !== 'GAME_IN_PROGRESS' && <PlayersPanel players= {players} me = {me} name = {name} />
-                }   
-                { 
-                    roomStatus === 'GAMEOVER' && gameOverPanel()
-                }   
             </div>
-
-            
-        )
-}
-
-
-function PlayersPanel(props){
-    // console.log(props); 
-    if(props.players === null){
-        return (<div>No players have connected</div>)
-    } else{
-        return (
-            <div className="readyUnitContainer">
-            {props.players.map((item,index) => {
-                let ready = null
-                let notReadyColor = '#E46258'
-                let readyColor = '#73C373'
-
-                if(item.isReady) {
-                    ready = <b>Ready!</b>
-                } else {
-                    ready = <b>Not Ready</b>
-                }
-                return(
-                    <div style={{backgroundColor: item.isReady ? readyColor : notReadyColor}} key={index}>
-                            <p >{index+1}. {item.name} {ready}</p>
-                    </div>
-                )
-                })
+            {
+                roomStatus === 'STARTED' &&
+                initialState !== null &&
+                <SocketContext.Provider value={socket}>
+                    <MainGame me={me} myName={name} initialState={initialState} />
+                </SocketContext.Provider>
+            }
+            {
+                roomStatus !== 'STARTED' && roomStatus !== 'GAME_IN_PROGRESS' && <PlayersPanel players={players} me={me} name={name} />
+            }
+            {
+                roomStatus === 'GAMEOVER' && gameOverPanel()
             }
         </div>
-        )   
-    }
+
+
+    )
 }
 
-// export default class Room extends Component {
 
-//     constructor(props) {
-//         super(props)
-    
-//         this.state = {
-//             roomCode: '',
-//         }
-//     }
+function PlayersPanel(props) {
+    if (props.players === null) {
+        return (<div>No players have connected</div>)
+    } else {
+        return (
+            <div className="readyUnitContainer">
+                {props.players.map((item, index) => {
+                    let ready = null
+                    let notReadyColor = '#E46258'
+                    let readyColor = '#73C373'
 
-//     render(){
-//         let roomInfo = null; 
-//         console.log(`${this.props.roomCode}`); 
-//         if (this.props.roomCode === ''){
-//             roomInfo = <h1>ERROR: not in a room</h1>
-//         } else{
-//             roomInfo = <h1>ROOM: {this.props.roomCode}</h1>
-//         }
-//         return(
-//             <div>
-//                 {roomInfo}
-//             </div>
-//         )
-//     }
-// }
+                    if (item.isReady) {
+                        ready = <b>Ready!</b>
+                    } else {
+                        ready = <b>Not Ready</b>
+                    }
+                    return (
+                        <div style={{ backgroundColor: item.isReady ? readyColor : notReadyColor }} key={index}>
+                            <p >{index + 1}. {item.name} {ready}</p>
+                        </div>
+                    )
+                })
+                }
+            </div>
+        )
+    }
+}
